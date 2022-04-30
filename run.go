d28b41b42906f582b89eb0dc73cd1f527dc4b5b1
Andrew W. Phillips <aphillips801@gmail.com>
2022-04-30 10:08:21 +1000

    rename time.go to types.go as a place for all eggql types (incl. custom scalars) move ID and TagHolder types there

--- run.go
+++ run.go
@@ -3,17 +3,11 @@ package eggql
 // run.go provides the MustRun function for quickly creating a GraphQL http handler
 
 import (
+	"net/http"
+
 	"github.com/andrewwphillips/eggql/internal/handler"
 	"github.com/andrewwphillips/eggql/internal/schema"
-	"net/http"
 )
-
-// TagHolder is used to declare a field with name "_" (underscore) in a struct to allow metadata (tags)
-// to be attached to a struct.  (Metadata can only be attached to fields, so we use an "_" field
-// to allow attaching metadata to the parent struct.)  This is currently just used to attach a
-// comment to Go structs that are used to generate a "description" associated with GraphQL
-// objects, interfaces and unions.
-type TagHolder struct{}
 
 // MustRun creates an http handler that handles GraphQL requests.  It takes up to 4
 // parameters. The 1st (optional) parameter is a map of string slices that contains
--- types.go
+++ types.go
@@ -1,16 +1,31 @@
 package eggql
 
-// TODO make one source file with all our custom scalars
-// time.go implements a GraphQL date/time type called "Time"
+// types.go has standard GraphQL types like "ID" as well as custom scalars like "Time"
 
 import (
 	"fmt"
 	"time"
 )
 
-const timeFormat = time.RFC3339 // GraphQL spec says that any "Time" ext. scalar type should use this format (ISO-8601)
+// ID is used when a standard GraphQL ID type is required.
+// An ID can be used like any other scalar (Int, etc) as a field type, resolver argument type etc.
+// It is typically used for a field that uniquely identifies an object, but it is up to the server
+// to guarantee uniqueness. It is stored as a string but can be encoded from an integer or string.
+type ID string
 
+// TagHolder is used to declare a field with name "_" (underscore) in a struct to allow metadata (tags)
+// to be attached to a struct.  (Metadata can only be attached to fields, so we use an "_" field
+// to allow attaching metadata to the parent struct.)  This is currently just used to attach a
+// comment to Go structs that are used to generate a "description" (in the GraphQL schema) for
+// objects, interfaces and unions.  See the "Star Wars" tutorial for an example.
+// Note: An empty struct will not add to the size of the containing struct if declared at the start.
+type TagHolder struct{}
+
+// Time is a custom scalar for representing a point in time
 type Time time.Time
+
+// timeFormat represents how a Time is encoded in a string
+const timeFormat = time.RFC3339 // GraphQL spec says that any "Time" ext. scalar type should use this format (ISO-8601)
 
 // UnmarshalEGGQL is called when eggql needs to decode a string to a Time
 func (pt *Time) UnmarshalEGGQL(in string) error {
